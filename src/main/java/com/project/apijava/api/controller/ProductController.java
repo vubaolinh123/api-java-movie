6edc6a4221f283c47a3f1b28501e88a96706337c
Linh Vũ <73287503+vubaolinh123@users.noreply.github.com>
2024-05-22 09:49:42 +0700

    first commit

---
+++ src/main/java/com/project/apijava/api/controller/ProductController.java
@@ -0,0 +1,59 @@
+package com.project.apijava.api.controller;
+
+import com.project.apijava.api.model.Product;
+import com.project.apijava.api.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable String id) {
+        Optional<Product> product = productService.getProductById(id);
+        return product.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable String id, @RequestBody Product productDetails) {
+        Optional<Product> product = productService.getProductById(id);
+        if (product.isPresent()) {
+            Product existingProduct = product.get();
+            existingProduct.setName(productDetails.getName());
+            existingProduct.setPrice(productDetails.getPrice());
+            Product updatedProduct = productService.saveProduct(existingProduct);
+            return ResponseEntity.ok(updatedProduct);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable String id) {
+        if (productService.getProductById(id).isPresent()) {
+            productService.deleteProduct(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/test/java/com/project/apijava/ApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.apijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
